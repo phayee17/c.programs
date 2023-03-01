4c56710e48fd88b7ba82058b35c1e467977adc37
phayee17 <123065450+phayee17@users.noreply.github.com>
2023-03-01 15:52:26 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+//sum of first 10 natural numbers
+#include <stdio.h>
+int main ()
+{
+  int i,sum=0;
+ for(i=1;i<=10; i++)
+{
+  printf("d ",i);
+  sum=sum+i;
+}
+ printf("\nThe sum is %d",sum);
+return 0;
+}
+	
